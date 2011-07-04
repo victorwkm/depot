eec80381fd4ca9f89fd0d227b63f52aaf0355b47
Kai Meng Wong <victorwkm@gmail.com>
2011-07-04 23:25:50 +1000

    catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
+  validates_presence_of :title, :message => "is toooo short"
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
@@ -6,4 +9,6 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, :length => { :minimum => 10 }
+  
 end
