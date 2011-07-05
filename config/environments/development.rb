0d2bfd64b7061375ac9722be4ad0f95f4ad474e5
Kai Meng Wong <victorwkm@gmail.com>
2011-07-06 03:03:33 +1000

    Revert 8fe0bfaf86aa2bc6064a1a72c32537287c72a8e9^..HEAD

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,8 @@ Depot::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
-  
+
   # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin  
+  config.action_dispatch.best_standards_support = :builtin
 end
 
