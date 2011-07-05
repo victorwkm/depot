b4d6d38c2f71ebfe87737bcfadfc69662d7ae81f
Kai Meng Wong <victorwkm@gmail.com>
2011-07-06 03:22:19 +1000

    change name to 5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Depot::Application.configure do
     :port => 587,
     :domain => "your.domain",
     :authentication => "plain",
-    :user_name => "myname4",
+    :user_name => "myname5",
     :password => "test",
     :enable_starttls_auto => true
   }
